d0f57c255a27d72156a9963a5c28fddd2ff9f92f
Ruben Dario Guzman Gonzalez <rubendario981@hotmail.com>
2023-03-08 20:12:14 -0500

    Data from API to get trend of movies is done

--- src/app/pages/trending/trending.component.ts
+++ src/app/pages/trending/trending.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Movie } from 'src/app/models/Movie';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-trending',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./trending.component.css']
 })
 export class TrendingComponent {
+  public listMoviesTrend!: Movie[]
 
+  constructor(private httpMovies: MoviesService){
+
+  }
+
+  ngOnInit(){
+    this.httpMovies.getMoviesTrend().subscribe(
+      response => this.listMoviesTrend = response.results
+    )
+  }
+
+  
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -6,7 +6,7 @@ import { environment } from '../../environments/environment.development'
 @Injectable({
   providedIn: 'root'
 })
-export class MoviesService {
+export class MoviesService {  
   public url: string;
   private API_KEY = environment.API_KEY_MOVIE_DB
 
@@ -30,8 +30,10 @@ export class MoviesService {
   getTopRatedMovies(): Observable<any> {
     return this.http.get(this.url + "/movie/top_rated?api_key=" + this.API_KEY)
   }
+  getMoviesTrend(): Observable<any> {
+    return this.http.get(this.url + "/trending/movie/day?api_key=" + this.API_KEY)
+  }
   getSimilarMovies(id: number): Observable<any> {
     return this.http.get(`${this.url}/movie/${id}/similar?api_key=${this.API_KEY}`)
   }
-
 }
